54175cf46b51d4afe18c5361d07de7f9a4821548
marieStamp <mariemarkina@gmail.com>
2020-11-24 04:19:31 +0300

    script.js changed

--- script.js
+++ script.js
@@ -1,83 +1,80 @@
+class Cart {
+    constructor(id, name, quantity, price, img) {
+        this.id = id;
+        this.name = name;
+        this. quantity = quantity;
+        this. price = price;
+        this.img = img;
+    }
+    addItem(){} /* метод добавляет товар в корзину */
+    removeItem(){}  /* метод удаляет товар из корзины */
+    saveCart(){}  /* метод сохраняет данные в корзине */
+    clearCart(){} /* метод удаляет все данные из корзины */
+    changeItemQuantity(){} /* метод меняет количество единиц одного товара */
+    getById(){} /* метод ищет элемент корзины по id товара */
+    addDelivery(){} /* метод добавляет способ доставки */
+    makeOrder(){} /* метод отправляет заказ в обработку */
+    goodsSum(){} /* метод рассчитывает общую стоимостль товаров в корзине */
+}
+
 class Goods {
-    constructor(name, color, size, price, quantity, shipping) {
+    constructor(id, name, color, size, price, quantity) {
+    this.id = id;
     this.name = name;
     this.color = color;
     this.size = size;
     this.price = price;
     this.quantity = quantity;
-    this.shipping = shipping;
     }
-    Add(){} /* метод добавляет товар в корзину */
-    Cancel(){}  /* метод удаляет товар из корзины */
-    Buy(){}  /* метод подтверждает покупку товара */
+    addToCart(){} /* добавляет товар в корзину */
+    removeFromCart(){} /* удаляет товар из корзины */
+    chooseColor(){} /* выбирает цвет товара */
+    chooseSize(){} /* выбирает цвет товара */
+    chooseQuantity(){} /* выбирает количество единиц товара */
 }
 
 class Man extends Goods {
-    constructor(name, color, size, price, quantity, shipping) {
-        super(name, color, size, price, quantity, shipping);
+    constructor(id, name, color, size, price, quantity) {
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
+    
 }
 
 class Women extends Goods {
-    constructor(name, color, size, price, quantity, shipping) {
-      super(name, color, size, price, quantity, shipping);  
+    constructor(id, name, color, size, price, quantity) {
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
 }
 
 class Kids extends Goods {
-    constructor(name, color, size, price, quantity, shipping) {
-        super(name, color, size, price, quantity, shipping);
+    constructor(id, name, color, size, price, quantity) {
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
 }
 
 class Accessories extends Goods {
-    constructor(name, color, size, price, quantity, shipping, type){
-        super(name, color, size, price, quantity, shipping);   /* добавляется тип аксессуара: сумки, шарфы, ремни и тд. */
+    constructor(id, name, color, size, price, quantity, type){
+        super(id, name, color, size, price, quantity);   /* добавляется тип аксессуара: сумки, шарфы, ремни и тд. */
         this.type = type;
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
 }
 
 class HotDeals extends Accessories {
-    constructor(name, color, size, price, quantity, shipping, type, discount) {
-        super(name, color, size, price, quantity, shipping, type);  /* размер скидки */
+    constructor(id, name, color, size, price, quantity, type, discount) {
+        super(id, name, color, size, price, quantity, type);  /* добавляем размер скидки */
         this.discount = discount;
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
     makeDiscount(){}  /* метод перессчитывает стоимость с учетом скидки */
 }
 
 class Featured extends HotDeals {
-    constructor(name, color, size, price, quantity, shipping, type, discount) {   /* в избранном могут быть товары, которые включают все свойства из каждого класса */
-        super(name, color, size, price, quantity, shipping, type, discount);
+    constructor(id, name, color, size, price, quantity, type, discount) {   /* в избранном могут быть товары, которые включают все свойства из каждого класса */
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();
 }
 
 class NewArrivals extends Accessories {
-    constructor(name, color, size, price, quantity, shipping, type) {   /* в этом разделе могут быть все свойства, кроме скидок */
-        super(name, color, size, price, quantity, shipping, type);
+    constructor(id, name, color, size, price, quantity, type) {   /* в этом разделе могут быть все свойства, кроме скидок */
     }
-    super.Add();
-    super.Cancel();
-    super.Buy();   
 }
 
-goodsSum(price, quantity){
-    return this.price * this.quantity
+
+function goodsSum(goodsArr){
+    return goodsArr.reduce((sum, current) => sum + current, 0);  /* goodsArr - массив стоимостей товаров, sum - сумма текущего и последующего товаров, current - стоимость текущего товара */
 }
